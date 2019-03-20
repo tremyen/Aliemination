afdeef9bb175d2bd1d3ea9be7afc54242e8c4e26
Manoel Neto <tremyen@gmail.com>
2019-03-20 12:03:10 -0300

    Vitória! Um sprite em ASM Z80!

--- sprite.asm
+++ sprite.asm
@@ -35,7 +35,7 @@ startProgram:
 
             ld a,15                     ; BRANCO (15)
             ld (FORCLR),a               ; cor da frente em branco
-            ld a,14                     ; CINZA (14)
+            ld a,15                     ; CINZA (14)
             ld (BAKCLR),a               ; cor de fundo
             LD a,1                      ; Preto (1)
             ld (BDRCLR),a               ; cor da borda
@@ -48,18 +48,18 @@ startProgram:
             ;você realmente mandou
 
             ; Preencher a VRAM
-            ; tabela de padroes
+            ; Block transfer to VRAM from memory
             ;-================================
-            ; A  - data byte
-            ; BC - length (8x8:8 16x16:32)
+            ; BC - blocklength
+            ; DE - Start address of VRAM
             ; HL - start address (entre 14.336 e 16.385)
             ; Ou seja, dependendo do tamanho utilizado você pode ter
             ; 256 ou 64 sprites
             ;-================================
-            ld a,sprite
             ld bc,32
-            ld hl,14336
-            call FILVRM
+            ld de,14336
+            ld hl,sprite
+            call LDIRVM
 
             ; putsprite
             ; -===========================-
@@ -72,7 +72,7 @@ startProgram:
             ld b,3
             ld hl,100
             ld a,80
-            ld d,10
+            ld d,11
             ld e,0
             call putSprite
 
@@ -95,10 +95,74 @@ SetScreen2:
             ret
 
 sprite:
-            DB 000,000,000,028,031,031,028,227
-            DB 031,227,031,227,031,224,000,000
-            DB 000,000,000,127,255,255,127,255
-            DB 255,255,255,255,255,127,000,000
+            ; color 11
+            DB 00000000b
+            DB 00000000b
+            DB 00000001b
+            DB 00000001b
+            DB 00000011b
+            DB 00010110b
+            DB 00011100b
+            DB 00011101b
+            DB 00111111b
+            DB 01111110b
+            DB 11111100b
+            DB 00011100b
+            DB 00001100b
+            DB 00001100b
+            DB 00000000b
+            DB 00000000b
+            DB 00000000b
+            DB 00000000b
+            DB 10000000b
+            DB 10000000b
+            DB 11000000b
+            DB 01101000b
+            DB 00111000b
+            DB 10111000b
+            DB 11111100b
+            DB 01111110b
+            DB 00111111b
+            DB 00111100b
+            DB 00110000b
+            DB 00110000b
+            DB 00000000b
+            DB 00000000b
+            ; --- Slot 0
+            ; color 1
+            DB 00000000b
+            DB 00000011b
+            DB 00000010b
+            DB 00000110b
+            DB 00111100b
+            DB 00101001b
+            DB 00100011b
+            DB 01100010b
+            DB 11000000b
+            DB 10000001b
+            DB 00000011b
+            DB 11100011b
+            DB 00110011b
+            DB 00010010b
+            DB 00011110b
+            DB 00000000b
+            DB 00000000b
+            DB 11000000b
+            DB 01000000b
+            DB 01100000b
+            DB 00111100b
+            DB 10010100b
+            DB 11000100b
+            DB 01000110b
+            DB 00000011b
+            DB 10000001b
+            DB 11000000b
+            DB 11000011b
+            DB 11001110b
+            DB 01001000b
+            DB 01111000b
+            DB 00000000b
+
 
 romPad:
             ds romSize-(romPad-romArea),0
